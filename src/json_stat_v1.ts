419f26828f95d81d38f9c3d62f9deb10579e07b0
Edwin de Jonge <edwindjonge@gmail.com>
2016-09-13 13:52:18 +0200

    json_stat_v1

--- src/json_stat_v1.ts
+++ src/json_stat_v1.ts
@@ -1,7 +1,10 @@
+// NOTE this is JsonStat interface version 1, which is at the time
+// of writing used by SCB, SSB and StatFin. 
+// Version 2 is better documented.
 export interface Dimension {
   label: string,
   category: {
-    index: {[id: string]: number},
+    index: {[id: string]: number} | string[],
     label: {[id: string]: string},
     unit?: {[id: string]: any}
   }
@@ -21,14 +24,29 @@ export interface JsonStat{
     label: string,
     source: string,
     updated: string,
-    values: number[] // or any?
+    value: number[] // or any?
   }
 }
 
 import {Schema, Field, DataPackage}  from './DataPackage';
 
-function is_a<T>(collection: T[]){
+//higher order utility function: creates a function that checks if it is part of a collection
+function is_a<T>(collection?: T[]){
+  if (!collection){
+    return ((value) => false);
+  }
   return ((value: T) => collection.indexOf(value) > -1);
+}
+
+function to_hash(index){
+  if (index.constructor === Array){
+    const hash = {};
+    for (let i in index){
+      hash[index[i]] = +i;
+    }
+    return hash;
+  }
+  return index;
 }
 
 function to_DataPackage(data: JsonStat, id?: string){
@@ -43,6 +61,10 @@ function to_DataPackage(data: JsonStat, id?: string){
 
   const dim = data.dataset.dimension;
   
+  if (!dim.role) {
+    dim.role = {metric: [], time: []};
+  }
+  
   //utility functions
   const is_a_metric = is_a(dim.role.metric);
   const is_time = is_a(dim.role.time);
@@ -50,14 +72,18 @@ function to_DataPackage(data: JsonStat, id?: string){
   for (let code of dim.id){
     const d: Dimension = dim[code];
     if (is_a_metric(code)){
-      const {index, label} = d.category;
+      let {index, label} = d.category;
+      index = to_hash(index);
+      label = label || {};
       for (let value in index){
         const field = { name: value, title: label[value], type: "number", unit: ""};
         fields.push(field);
       }      
     } else {
       const field = { name: code, title: d.label, categories: [], type: "categorical" };
-      const {index, label} = d.category;
+      let {index, label} = d.category;
+      index = to_hash(index);
+      label = label || {};
       for (let value in index){
         field.categories[index[value]] = {
           name: value,
@@ -77,8 +103,41 @@ export function to_data(data: JsonStat, dp?: DataPackage){
   if (!dp){
     dp = to_DataPackage(data);
   }
+  const value = data.dataset.value;
+  const size = data.dataset.dimension.size;
+  
+  
+  for (let i in value){
+  }
+  return dp;
 }
 
-const jstat:JsonStat = require("../examples/jsonstat.json");
-let dp = to_DataPackage(jstat, "hello");
-console.log(JSON.stringify(dp));
+// const jstat:JsonStat = require("../examples/jsonstat.json");
+// let dp = to_DataPackage(jstat, "hello");
+// console.log(JSON.stringify(dp));
+
+const j_data:JsonStat = require("../examples/order_v1.json");
+
+const data = to_data(j_data);
+console.log(JSON.stringify(data));
+
+const idx = 5;
+const size = [2, 3];
+
+let c_size = size.slice().reverse();
+let factor = 1;
+for (let i in c_size){
+  const f = factor * c_size[i];
+  c_size[i] = factor;
+  factor = f;
+}
+c_size = c_size.reverse();
+
+let index = [];
+for (let i in size){
+  index[i] = Math.floor(idx/c_size[i]) % size[i];
+  console.log("i", size[i], c_size[i])
+}
+
+//console.log(c_size);
+//console.log(index);
